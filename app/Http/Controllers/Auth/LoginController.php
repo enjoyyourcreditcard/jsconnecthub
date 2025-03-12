f8eb9c0967923d61b589f9b8ae4dc5f9ea108170
enjoyyourcreaditcard <joywidiwibowo@gmail.com>
2025-03-12 17:48:34 +0700

    feat: login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,7 +3,13 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class LoginController extends Controller
 {
@@ -36,5 +42,38 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
         $this->middleware('auth')->only('logout');
+    }
+
+    /**
+     * Login proses and generate token.
+     *
+     * @return JsonResponse
+     */
+    public function login(Request $request)
+    {
+        $validatedData = $request->validate([
+            'email'     => ['required', 'email'],
+            'password'  => ['required', 'string']
+        ]);
+
+        $user = User::where('email', $validatedData['email'])->first();
+
+        if (!$user || !Hash::check($validatedData['password'], $user->password)) {
+            throw new HttpResponseException(response([
+                "errors" => [
+                    "message" => [
+                        "Email or password is wrong"
+                    ]
+                ]
+            ], 401));
+        }
+
+        $user->tokens()->delete();
+
+        $user->save();
+
+        $tokenString = $user->first_name . "-" . $user->email . "-" . $user->username . "_" . $user->id;
+
+        return response()->json(['status' => true, 'result' => $user, 'token' => $user->createToken($tokenString)->plainTextToken]);
     }
 }
